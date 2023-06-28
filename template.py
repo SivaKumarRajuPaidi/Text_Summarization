36fbf2234b1fc68b7a52052c88c61cf342cefb95
MrSivakumarraju <sivakumarraju.skr@gmail.com>
2023-06-28 16:40:01 +0530

    requirements added

--- template.py
+++ template.py
@@ -4,20 +4,20 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "textsummarizer"
+project_name = "textSummarizer"
 
 list_of_files = [
     ".github.com/workflows/gitkeep",
-    f"scr/{project_name}/__init__.py",
-    f"scr/{project_name}/components/__init__.py",
-    f"scr/{project_name}/utils/__init__.py",
-    f"scr/{project_name}/utils/common.py",
-    f"scr/{project_name}/logging/__init__.py",
-    f"scr/{project_name}/config/__init__.py",
-    f"scr/{project_name}/config/configuration.py",
-    f"scr/{project_name}/pipeline/__init__.py",
-    f"scr/{project_name}/entity/__init__.py",
-    f"scr/{project_name}/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "parms.yaml",
     "app.py",
